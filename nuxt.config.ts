1f8a98f818ea2d3a26cd58cc233d4ba1a645eab2
jebessad <jebessadejene2021@gmail.com>
2024-11-08 22:35:51 +0300

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,  // Disable SSR for static deployment
+  app: {
+    baseURL: '/',  // Set the base URL to the root of the repository
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
+  // Add Tailwind CSS and PostCSS config
   css: ['@/assets/css/tailwind.css'],
+  
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
-      
     },
   },
 })
